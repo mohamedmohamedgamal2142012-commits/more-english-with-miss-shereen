2297ddaceba705ca61db8cc6c02418b1c7946355
Mohamed <mohamed@example.com>
2026-07-29 21:38:54 +0300

    Fix extra closing div causing build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,7 +234,6 @@ export default function HomePage() {
                     </div>
                   </div>
                 ))}
-              </div>
             </div>
           </div>
         </div>
